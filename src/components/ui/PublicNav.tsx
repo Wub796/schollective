4e1272850395a73f2b753af0074a58c1e13b9f3b
Ben <wub67608@gmail.com>
2026-07-24 14:58:46 -0500

    style: move Schollective logo further inward on PublicNav menu bar

--- src/components/ui/PublicNav.tsx
+++ src/components/ui/PublicNav.tsx
@@ -143,7 +143,7 @@ export function PublicNav() {
           }`}
         >
           {/* COLUMN 1: LEFT (Logo) */}
-          <div className="flex items-center justify-start pl-6">
+          <div className="flex items-center justify-start pl-9">
             <Link
               href="/"
               data-nav-item="true"
@@ -167,7 +167,7 @@ export function PublicNav() {
           </div>
 
           {/* COLUMN 3: RIGHT (Log In + Mobile Hamburger) */}
-          <div className="flex items-center justify-end pr-6">
+          <div className="flex items-center justify-end pr-9">
             <div className="hidden lg:flex items-center">
               <NavItem label="Log In" href="/login" active={pathname === "/login"} />
             </div>
